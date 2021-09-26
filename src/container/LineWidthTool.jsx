6beefc6f7dad4d4d8864432582029287a859c42b
Ricardo Cuan <rcuan@ayudinga.org>
2021-09-25 20:46:38 -0500

    🐛 BUGFIX: CRA not building bug

--- src/container/LineWidthTool.jsx
+++ src/container/LineWidthTool.jsx
@@ -1,12 +1,7 @@
-import { useCanvas } from '../components/CanvasContext'
 import ButtonWidth from '../components/ButtonWidth'
 import '../styles/LineWidthTool.css'
 
 const LineWidthTool = () => {
-  const {
-    lineWidth,
-    setLineWidth
-  } = useCanvas()
   return (
     <div className='line-width'>
       <ButtonWidth size='1' />
@@ -15,15 +10,6 @@ const LineWidthTool = () => {
       <ButtonWidth size='6' />
       <ButtonWidth size='8' />
       <ButtonWidth size='10' />
-      {/* <input
-           type="number"
-           name="draw"
-           min="1"
-           max="10"
-           step="1"
-           defaultValue={lineWidth}
-           onChange={e => setLineWidth(e.target.value)}
-         /> */}
     </div>
   )
 }
